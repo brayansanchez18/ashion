358139cb35115fe275bac2259224834a62290510
brayansanchez18 <brayan.sanchez.contacto@gmail.com>
2021-07-23 14:57:02 -0500

    the featured products module has been made dynamic

--- frontend/vistas/modulos/productos.php
+++ frontend/vistas/modulos/productos.php
@@ -1,267 +1,91 @@
-<section class="product spad">
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-4 col-md-4">
-        <div class="section-title">
-          <h4>PRODUCTOS NUEVOS</h4>
+<?php
+
+$titulosModulos = array("ARTÍCULOS RECIENTES", "LO MÁS VENDIDO", "LO MÁS VISTO");
+$rutaModulos = array("articulos-recientes","lo-mas-vendido","lo-mas-visto");
+
+$base = 0;
+$tope = 8;
+
+if($titulosModulos[0] == "ARTÍCULOS RECIENTES"){
+  $ordenar = "id";
+  $item = 'estado';
+  $valor = 1;
+  $modo = 'DESC';
+  $recientes = ControladorProductos::ctrMostrarProductos($ordenar, $item, $valor, $base, $tope, $modo);
+}
+
+if($titulosModulos[1] == "LO MÁS VENDIDO") {
+  $ordenar = "ventas";
+  $item = 'estado';
+  $valor = 1;
+  $modo = 'DESC';
+  $ventas = ControladorProductos::ctrMostrarProductos($ordenar, $item, $valor, $base, $tope, $modo);
+}
+
+if($titulosModulos[2] == "LO MÁS VISTO") {
+  $ordenar = "vistas";
+  $item = 'estado';
+  $valor = 1;
+  $modo = 'DESC';
+  $vistas = ControladorProductos::ctrMostrarProductos($ordenar, $item, $valor, $base, $tope, $modo);
+}
+
+$modulos = array($recientes, $ventas, $vistas);
+
+?>
+
+<?php for ($i=0; $i < count($titulosModulos); $i++): ?>
+  <section class="product spad">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4 col-md-4">
+          <div class="section-title">
+            <h4><a href="<?=$frontend.$rutaModulos[$i]?>" class="linkdestacados"><?=$titulosModulos[$i]?></a></h4>
+          </div>
         </div>
       </div>
+
+      <?php if ($modulos[$i]): ?>
+        <div class="row property__gallery">
+
+          <?php foreach ($modulos[$i] as $key => $value): ?>
+            <?php if ($value['estado'] != 0): ?>
+              <div class="col-lg-3 col-md-4 col-sm-6 mix">
+                <?php if ($value['oferta'] != 0): ?>
+                <div class="product__item sale">
+                <?php else: ?>
+                <div class="product__item">
+                <?php endif ?>
+                  <div class="product__item__pic set-bg" data-setbg="<?=$backend.$value['portada']?>">
+                    <?php if ($value['oferta'] != 0): ?>
+                      <div class="label sale">En Oferta</div>
+                    <?php endif ?>
+                    <?php if ($value['stock'] == 0): ?>
+                      <div class="label stockout stockblue">Sin Stock</div>
+                    <?php endif ?>
+                  </div>
+
+                  <div class="product__item__text">
+                    <h6><a href="<?=$frontend.$value['ruta']?>"><?=$value['titulo']?></a></h6>
+                    <hr>
+                    <?php if ($value['precio'] == 0): ?>
+                      <div class="product__price">GRATIS</div>
+                    <?php else: ?>
+                      <?php if ($value['oferta'] != 0): ?>
+                        <div class="product__price">$ <?=$value['precioOferta']?> USD <span>$ <?=$value['precio']?></span></div>
+                      <?php else: ?>
+                        <div class="product__price">$ <?=$value['precio']?> USD</div>
+                      <?php endif ?>
+                    <?php endif ?>
+                  </div>
+                </div>
+              </div>
+            <?php endif ?>
+          <?php endforeach ?>
+        </div>
+      <?php else: ?>
+        <div class="col-xs-12 error404 text-center"><h2>¡Oops! Aùn no hay productos en esta secciòn</h2></div>
+      <?php endif ?>
     </div>
-
-    <div class="row property__gallery">
-      <div class="col-lg-3 col-md-4 col-sm-6 mix">
-        <div class="product__item">
-          <div class="product__item__pic set-bg" data-setbg="<?=$frontend?>vistas/img/product/product-1.jpg">
-            <div class="label new">New</div>
-            <div class="label sale">Sale</div>
-          </div>
-
-          <div class="product__item__text">
-            <h6><a href="#">Buttons tweed blazer</a></h6>
-            <hr>
-            <div class="product__price">$5,000 MXN</div>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-lg-3 col-md-4 col-sm-6 mix">
-        <div class="product__item">
-          <div class="product__item__pic set-bg" data-setbg="<?=$frontend?>vistas/img/product/product-1.jpg">
-            <div class="label new">New</div>
-            <div class="label sale">Sale</div>
-          </div>
-
-          <div class="product__item__text">
-            <h6><a href="#">Buttons tweed blazer</a></h6>
-            <hr>
-            <div class="product__price">$5,000 MXN</div>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-lg-3 col-md-4 col-sm-6 mix">
-        <div class="product__item">
-          <div class="product__item__pic set-bg" data-setbg="<?=$frontend?>vistas/img/product/product-1.jpg">
-            <div class="label new">New</div>
-            <div class="label sale">Sale</div>
-          </div>
-
-          <div class="product__item__text">
-            <h6><a href="#">Buttons tweed blazer</a></h6>
-            <hr>
-            <div class="product__price">$5,000 MXN</div>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-lg-3 col-md-4 col-sm-6 mix">
-        <div class="product__item">
-          <div class="product__item__pic set-bg" data-setbg="<?=$frontend?>vistas/img/product/product-1.jpg">
-            <div class="label new">New</div>
-            <div class="label sale">Sale</div>
-          </div>
-
-          <div class="product__item__text">
-            <h6><a href="#">Buttons tweed blazer</a></h6>
-            <hr>
-            <div class="product__price">$5,000 MXN</div>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-lg-3 col-md-4 col-sm-6 mix">
-        <div class="product__item">
-          <div class="product__item__pic set-bg" data-setbg="<?=$frontend?>vistas/img/product/product-1.jpg">
-            <div class="label new">New</div>
-            <div class="label sale">Sale</div>
-          </div>
-
-          <div class="product__item__text">
-            <h6><a href="#">Buttons tweed blazer</a></h6>
-            <hr>
-            <div class="product__price">$5,000 MXN</div>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-lg-3 col-md-4 col-sm-6 mix">
-        <div class="product__item">
-          <div class="product__item__pic set-bg" data-setbg="<?=$frontend?>vistas/img/product/product-1.jpg">
-            <div class="label new">New</div>
-            <div class="label sale">Sale</div>
-          </div>
-
-          <div class="product__item__text">
-            <h6><a href="#">Buttons tweed blazer</a></h6>
-            <hr>
-            <div class="product__price">$5,000 MXN</div>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-lg-3 col-md-4 col-sm-6 mix">
-        <div class="product__item">
-          <div class="product__item__pic set-bg" data-setbg="<?=$frontend?>vistas/img/product/product-1.jpg">
-            <div class="label new">New</div>
-            <div class="label sale">Sale</div>
-          </div>
-
-          <div class="product__item__text">
-            <h6><a href="#">Buttons tweed blazer</a></h6>
-            <hr>
-            <div class="product__price">$5,000 MXN</div>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-lg-3 col-md-4 col-sm-6 mix">
-        <div class="product__item">
-          <div class="product__item__pic set-bg" data-setbg="<?=$frontend?>vistas/img/product/product-1.jpg">
-            <div class="label new">New</div>
-            <div class="label sale">Sale</div>
-          </div>
-
-          <div class="product__item__text">
-            <h6><a href="#">Buttons tweed blazer</a></h6>
-            <hr>
-            <div class="product__price">$5,000 MXN</div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
-
-<section class="product spad">
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-4 col-md-4">
-        <div class="section-title">
-          <h4>PRODUCTOS MÁS VENDIDOS</h4>
-        </div>
-      </div>
-    </div>
-
-    <div class="row property__gallery">
-      <div class="col-lg-3 col-md-4 col-sm-6 mix">
-        <div class="product__item">
-          <div class="product__item__pic set-bg" data-setbg="<?=$frontend?>vistas/img/product/product-1.jpg">
-            <div class="label new">New</div>
-            <div class="label sale">Sale</div>
-          </div>
-
-          <div class="product__item__text">
-            <h6><a href="#">Buttons tweed blazer</a></h6>
-            <hr>
-            <div class="product__price">$5,000 MXN</div>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-lg-3 col-md-4 col-sm-6 mix">
-        <div class="product__item">
-          <div class="product__item__pic set-bg" data-setbg="<?=$frontend?>vistas/img/product/product-1.jpg">
-            <div class="label new">New</div>
-            <div class="label sale">Sale</div>
-          </div>
-
-          <div class="product__item__text">
-            <h6><a href="#">Buttons tweed blazer</a></h6>
-            <hr>
-            <div class="product__price">$5,000 MXN</div>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-lg-3 col-md-4 col-sm-6 mix">
-        <div class="product__item">
-          <div class="product__item__pic set-bg" data-setbg="<?=$frontend?>vistas/img/product/product-1.jpg">
-            <div class="label new">New</div>
-            <div class="label sale">Sale</div>
-          </div>
-
-          <div class="product__item__text">
-            <h6><a href="#">Buttons tweed blazer</a></h6>
-            <hr>
-            <div class="product__price">$5,000 MXN</div>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-lg-3 col-md-4 col-sm-6 mix">
-        <div class="product__item">
-          <div class="product__item__pic set-bg" data-setbg="<?=$frontend?>vistas/img/product/product-1.jpg">
-            <div class="label new">New</div>
-            <div class="label sale">Sale</div>
-          </div>
-
-          <div class="product__item__text">
-            <h6><a href="#">Buttons tweed blazer</a></h6>
-            <hr>
-            <div class="product__price">$5,000 MXN</div>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-lg-3 col-md-4 col-sm-6 mix">
-        <div class="product__item">
-          <div class="product__item__pic set-bg" data-setbg="<?=$frontend?>vistas/img/product/product-1.jpg">
-            <div class="label new">New</div>
-            <div class="label sale">Sale</div>
-          </div>
-
-          <div class="product__item__text">
-            <h6><a href="#">Buttons tweed blazer</a></h6>
-            <hr>
-            <div class="product__price">$5,000 MXN</div>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-lg-3 col-md-4 col-sm-6 mix">
-        <div class="product__item">
-          <div class="product__item__pic set-bg" data-setbg="<?=$frontend?>vistas/img/product/product-1.jpg">
-            <div class="label new">New</div>
-            <div class="label sale">Sale</div>
-          </div>
-
-          <div class="product__item__text">
-            <h6><a href="#">Buttons tweed blazer</a></h6>
-            <hr>
-            <div class="product__price">$5,000 MXN</div>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-lg-3 col-md-4 col-sm-6 mix">
-        <div class="product__item">
-          <div class="product__item__pic set-bg" data-setbg="<?=$frontend?>vistas/img/product/product-1.jpg">
-            <div class="label new">New</div>
-            <div class="label sale">Sale</div>
-          </div>
-
-          <div class="product__item__text">
-            <h6><a href="#">Buttons tweed blazer</a></h6>
-            <hr>
-            <div class="product__price">$5,000 MXN</div>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-lg-3 col-md-4 col-sm-6 mix">
-        <div class="product__item">
-          <div class="product__item__pic set-bg" data-setbg="<?=$frontend?>vistas/img/product/product-1.jpg">
-            <div class="label new">New</div>
-            <div class="label sale">Sale</div>
-          </div>
-
-          <div class="product__item__text">
-            <h6><a href="#">Buttons tweed blazer</a></h6>
-            <hr>
-            <div class="product__price">$5,000 MXN</div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
+  </section>
+<?php endfor ?>
